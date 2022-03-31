a93c5fbd4c522869fb2371e1f435854d61147692
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2022-04-01 00:59:06 +0200

    Touch My Heap ONI-CHAAAAAAAAAAN !!!!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,34 @@
-import Sorting.SortingStrategies.QuickSort;
 import Sorting.Sorting;
+import Sorting.SortingStrategies.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class Main {
+    static List<Integer> first, second = new ArrayList<>();
+    static double start;
+
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>() {
-            {
-                add(8);
-                add(2);
-                add(4);
-                add(1);
-                add(3);
-            }
-        };
 
-        Sorting.Sort(arrayList, new QuickSort<>(), Comparator.reverseOrder());
+        for (int size = 10000; size <= 100000; size += 1000) {
+            first = Randomizer.random(size);
+            System.out.println("Input : " + size);
+            sort(new BubbleSort<>() , "Bubble");
+            sort(new SelectionSort<>() , "Selection");
+            sort(new InsertionSort<>() , "Insertion");
+            sort(new HeapSort<>() , "Heap");
+            sort(new MergeSort<>() , "Merge");
+            sort(new QuickSort<>() , "Quick");
+            System.out.println();
+        }
 
-        System.out.println(arrayList);
+    }
+
+    public static void sort(SortStrategy<Integer> sortStrategy, String name) {
+        second = new ArrayList<>(first) ;
+        start = System.currentTimeMillis();
+        Sorting.Sort(second, sortStrategy , Comparator.reverseOrder());
+        System.out.println(name + " Sort : " + (System.currentTimeMillis() - start) + " ms");
     }
 }
