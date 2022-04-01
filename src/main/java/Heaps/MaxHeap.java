01b9fe95cad82ae35b92ea094ca17c32ca7319ed
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-04-01 02:03:09 +0200

    Analysis Recording

--- src/main/java/Heaps/MaxHeap.java
+++ src/main/java/Heaps/MaxHeap.java
@@ -1,87 +1,147 @@
 package Heaps;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MaxHeap<T extends Comparable<T>> {
-        private List<T> items = new ArrayList<>();
+    ArrayList<T> items = new ArrayList<>();
+    private int size = 0;
 
-        //we can use items.size() instead
-        private int size = 0;
-
-        /**
-         * @param val the value to inserted
-         */
-        public void insert(T val) {
-            //percolate down
-            //by making hole in the last position in the heap and bubble it up until root or its right position
-            //better than bubbling up by swapping since bubble up takes 3 assignments every bubble
-            //this algorithm takes only one
-            int hole = size;
-            for (items.add(val); val.compareTo(items.get((hole - 1) / 2)) > 0 && hole > 0; hole = (hole - 1) / 2)
-                items.set(hole, items.get((hole - 1) / 2));
-            items.set(hole, val);
-
-            size++;
-
-//        //fix up the new node by regular bubble up
-//        BubbleUp(size -1);
-        }
-
-        /**
-         * @return the largest element in the entire heap
-         */
-        public T remove() {
-            if (isEmpty())
-                throw new IllegalStateException();
-
-            T root = items.get(0);
-            items.set(0, items.get(--size));
-            items.remove(size);
-
-            //using same technique as insert
-            //using percolate down with a hole to reduce number of assignments
-            percolateDown();
-
-            //fix the heap from the root
-//        BubbleDown(0);
-
-            return root;
-        }
-
-        private void percolateDown() {
-            //if the removed node was the last one in the heap
-            if (size == 0) return;
-
-            int child, hole = 0;
-            T temp = items.get(0);
-
-            for (; hole * 2 + 1 < size; hole = child) {
-                //the next 3 line to compare between 2 children ang get the largest
-                child = hole * 2 + 1;
-                if (child + 1 < size && items.get(child).compareTo(items.get(child + 1)) < 0)
-                    child++;
-
-                //if largest child is lower than the temp value then stop digging down
-                //as we are in the right position
-                if (items.get(child).compareTo(temp) < 0)
-                    break;
-                else
-                    items.set(hole, items.get(child));
-            }
-            items.set(hole, temp);
-        }
-
-        public int size() {
-            return size;
-        }
-
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        public T Max(){
-            return items.get(0) ;
-        }
+    /***
+     * Returns true if this heap contains no elements.
+     * @return true if the heap is empty.
+     */
+    public boolean isEmpty() {
+        return size == 0;
     }
+
+    /***
+     * Inserts new item into the heap.
+     * @param value value to be inserted.
+     */
+    public void insert(T value) {
+        items.add(value);
+        size++;
+
+        percolateUp();
+    }
+
+    /***
+     * Returns and remove the root of the heap (max item).
+     * @return max item in the heap.
+     */
+    public T remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        T root = items.get(0);
+        items.set(0, items.get(--size));
+        items.remove(size);
+
+        percolateDown();
+        return root;
+    }
+
+    /***
+     * Returns max item in the heap.
+     * @return max element in the heap.
+     */
+    public T max() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return items.get(0);
+    }
+
+    /***
+     * Maintains the heap properties by after inserting operation.
+     */
+    private void percolateUp() {
+        T newNode = items.get(size - 1);
+        int hole = size - 1;
+
+        while (newNode.compareTo(items.get((hole - 1) / 2)) > 0 && hole > 0) {
+            items.set(hole, items.get((hole - 1) / 2));
+
+            hole = (hole - 1) / 2;
+        }
+        items.set(hole, newNode);
+    }
+
+    /***
+     * Maintains the heap properties after deletion operation.
+     */
+    private void percolateDown() {
+        if (size == 0)
+            return;
+
+        int child, hole = 0;
+        T temp = items.get(0);
+
+        while (hole * 2 + 1 < size) {
+            child = hole * 2 + 1;
+
+            if (child + 1 < size && compare(child, child + 1) < 0)
+                child++;
+
+            if (items.get(child).compareTo(temp) < 0)
+                break;
+            else
+                items.set(hole, items.get(child));
+
+            hole = child;
+        }
+        items.set(hole, temp);
+    }
+
+    /***
+     * Compares two items and returns integer value which indicates if the first item is greater, smaller or equal the second item.
+     * @param first first item.
+     * @param second second item.
+     * @return integer value which indicates if the first item is greater, smaller or equal the second item.
+     */
+    private int compare(int first, int second) {
+        return getItem(first).compareTo(getItem(second));
+    }
+
+
+    /***
+     * Returns item at a desired index.
+     * @param index desired index.
+     * @return the item at the desired index.
+     */
+    private T getItem(int index) {
+        return items.get(index);
+    }
+
+    /***
+     * Returns true if this array is a heap.
+     * @param array array with integer values.
+     * @return true if this array is a heap.
+     */
+    public static boolean isMaxHeap(int[] array) {
+        return isMaxHeap(array, 0);
+    }
+
+    /***
+     * Implementation of checking if the array is a max heap or not (recursive).
+     * @param array array with integer values.
+     * @param index index of the next item.
+     * @return true if the array is a heap
+     */
+    private static boolean isMaxHeap(int[] array, int index) {
+        int lastParentIndex = (array.length - 2) / 2;
+        if (index > lastParentIndex)
+            return true;
+
+        int leftChildIndex = index * 2 + 1;
+        int rightChildIndex = index * 2 + 2;
+
+        boolean isValidParent = array[index] >= array[leftChildIndex] &&
+                                array[index] >= array[rightChildIndex];
+
+        return isValidParent &&
+               isMaxHeap(array, leftChildIndex) &&
+               isMaxHeap(array, rightChildIndex);
+    }
+}
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import Sorting.Sorting;
 import Sorting.SortingStrategies.*;
+import com.opencsv.CSVWriter;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,27 +12,45 @@ import java.util.List;
 public class Main {
     static List<Integer> first, second = new ArrayList<>();
     static double start;
+    static CSVWriter writer;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/main/resources/Data.csv");
+        FileWriter outputFile = new FileWriter(file);
+        writer = new CSVWriter(outputFile);
+
+        String[] header = {"Input Size", "Bubble Sort", "Selection Sort", "Insertion Sort",
+                "Heap Sort", "Merge Sort", "Quick Sort"};
+        writer.writeNext(header);
 
         for (int size = 10000; size <= 100000; size += 1000) {
             first = Randomizer.random(size);
             System.out.println("Input : " + size);
-            sort(new BubbleSort<>() , "Bubble");
-            sort(new SelectionSort<>() , "Selection");
-            sort(new InsertionSort<>() , "Insertion");
-            sort(new HeapSort<>() , "Heap");
-            sort(new MergeSort<>() , "Merge");
-            sort(new QuickSort<>() , "Quick");
+            double bubbleTime = sort(new BubbleSort<>(), "Bubble");
+            double selectionTime = sort(new SelectionSort<>(), "Selection");
+            double insertionTime = sort(new InsertionSort<>(), "Insertion");
+            double heapTime = sort(new HeapSort<>(), "Heap");
+            double mergeTime = sort(new MergeSort<>(), "Merge");
+            double quickTime = sort(new QuickSort<>(), "Quick");
+
+            String[] data = {String.valueOf(size),
+                    String.valueOf(bubbleTime), String.valueOf(selectionTime), String.valueOf(insertionTime),
+                    String.valueOf(heapTime), String.valueOf(mergeTime), String.valueOf(quickTime)};
+            writer.writeNext(data);
+
             System.out.println();
         }
-
+        writer.close();
     }
 
-    public static void sort(SortStrategy<Integer> sortStrategy, String name) {
-        second = new ArrayList<>(first) ;
+    public static double sort(SortStrategy<Integer> sortStrategy, String name) {
+        second = new ArrayList<>(first);
         start = System.currentTimeMillis();
-        Sorting.Sort(second, sortStrategy , Comparator.reverseOrder());
-        System.out.println(name + " Sort : " + (System.currentTimeMillis() - start) + " ms");
+        Sorting.Sort(second, sortStrategy, Comparator.reverseOrder());
+
+        double time = System.currentTimeMillis() - start;
+        System.out.println(name + " Sort : " + time + " ms");
+
+        return time;
     }
 }
