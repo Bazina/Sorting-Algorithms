7a80e4e502fb13874bdea2fd6b41627b78970d63
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2022-04-20 16:08:39 +0200

    Refactoring

---
+++ src/main/java/Main/CanvasUtils.java
@@ -0,0 +1,159 @@
+package Main;
+
+import Main.Controller.Move;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+import java.util.List;
+import java.util.Queue;
+
+public class CanvasUtils {
+    private final Canvas theCanvas;
+    private final GraphicsContext gc;
+    private double delay;
+    private double blockSize;
+    private boolean strokeExist;
+    private List<Integer> arrayList;
+    private boolean sorting;
+
+    public CanvasUtils(Canvas theCanvas) {
+        this.theCanvas = theCanvas;
+        gc = theCanvas.getGraphicsContext2D();
+        this.delay = 0.01 ;
+    }
+
+    public void drawArray() {
+        gc.setFill(Color.BLACK);
+
+        if (strokeExist) gc.setStroke(Color.rgb(244, 244, 244));
+        else gc.setStroke(Color.BLACK);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            gc.fillRect(i * blockSize, 0, blockSize, arrayList.get(i));
+            gc.strokeRect(i * blockSize, 0, blockSize, arrayList.get(i));
+        }
+    }
+
+    public void startDrawSorting(Queue<Move> moves) {
+        Timeline timeline1;
+        Timeline timeline2;
+
+        if (moves.isEmpty()) return;
+
+        Move current = moves.poll();
+        if (current.pivot) timeline1 = coloredTimeLine(current, Color.GOLD);
+        else timeline1 = coloredTimeLine(current, Color.RED);
+
+        gc.setFill(Color.BLACK);
+        timeline2 = coloredTimeLine(current, Color.BLACK);
+        Timeline finalTimeline = timeline2;
+        timeline1.setOnFinished(e -> finalTimeline.play());
+
+        timeline1.play();
+
+        while (!moves.isEmpty()) {
+            current = moves.poll();
+
+            if (current.pivot) timeline1 = coloredTimeLine(current, Color.GOLD);
+            else timeline1 = coloredTimeLine(current, Color.RED);
+
+            Timeline finalTimeline1 = timeline1;
+            timeline2.setOnFinished(e -> finalTimeline1.play());
+
+            timeline2 = coloredTimeLine(current, Color.BLACK);
+            Timeline finalTimeline2 = timeline2;
+            timeline1.setOnFinished(e -> finalTimeline2.play());
+        }
+        timeline2.setOnFinished(e -> end());
+    }
+
+    public void end() {
+        if (strokeExist) gc.setStroke(Color.rgb(244, 244, 244));
+        else gc.setStroke(Color.GREEN);
+
+        Timeline timeline1;
+        Timeline timeline2;
+
+        gc.setFill(Color.GREEN);
+        timeline1 = coloredTimeLine(new Move(0, 0, arrayList.get(0), 0, false, true), Color.GREEN);
+        timeline2 = coloredTimeLine(new Move(1, 0, arrayList.get(1), 0, false, true), Color.GREEN);
+        Timeline finalTimeline = timeline2;
+        timeline1.setOnFinished(e -> finalTimeline.play());
+        timeline1.play();
+
+        for (int i = 2; i < arrayList.size(); i++) {
+            timeline1 = coloredTimeLine(new Move(i, 0, arrayList.get(i), 0, false, true), Color.GREEN);
+            Timeline finalTimeline1 = timeline1;
+            timeline2.setOnFinished(e -> finalTimeline1.play());
+            timeline2 = timeline1;
+        }
+        sorting = false;
+    }
+
+    public Timeline coloredTimeLine(Move current, Color color) {
+        if (current.singleCol) {
+            return new Timeline(
+                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, theCanvas.getHeight())),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightI))
+            );
+        } else if (current.swap) {
+            return new Timeline(
+                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.j * blockSize, 0, blockSize, current.heightJ)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.j * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightJ)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.j * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightJ))
+            );
+        } else {
+            return new Timeline(
+                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.j * blockSize, 0, blockSize, current.heightJ)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightI)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.j * blockSize, 0, blockSize, current.heightJ)),
+                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.j * blockSize, 0, blockSize, current.heightJ))
+            );
+        }
+    }
+
+    public void setBlockSize(double blockSize) {
+        this.blockSize = blockSize;
+    }
+
+    public void setStrokeExist(boolean strokeExist) {
+        this.strokeExist = strokeExist;
+    }
+
+    public void setData(List<Integer> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public void clear() {
+        gc.clearRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
+    }
+
+    public double getDelay() {
+        return delay;
+    }
+
+    public void setDelay(double delay) {
+        this.delay = delay;
+    }
+
+    public boolean isSorting() {
+        return sorting;
+    }
+
+    public void setSorting(boolean sorting) {
+        this.sorting = sorting;
+    }
+}
--- src/main/java/Main/MainController.java
+++ src/main/java/Main/MainController.java
@@ -5,17 +5,12 @@ import Main.Controller.Move;
 import Main.Implementation.Sorting.Sorting;
 import Main.Implementation.Sorting.SortingStrategies.MergeSort;
 import Main.Implementation.Sorting.SortingStrategies.SortAttributes;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
 
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
@@ -24,6 +19,11 @@ import java.net.URLClassLoader;
 import java.util.*;
 
 public class MainController implements Initializable {
+    private final String[] sorts = {"BinaryInsertionSort", "BitonicSort", "BogoSort", "BubbleSort",
+            "CocktailShakerSort", "CombSort", "CountingSort", "DoubleSelectionSort", "GnomeSort",
+            "HeapSort", "InPlaceMergeSort", "InsertionSort", "MergeSort", "OddEvenSort",
+            "OptimizedBubbleSort", "PancakeSort", "QuickSort", "SelectionSort", "ShellSort", "StoogeSort"};
+    CanvasUtils canvasUtils;
     @FXML
     private Canvas theCanvas;
     @FXML
@@ -32,22 +32,9 @@ public class MainController implements Initializable {
     private TextField dataSize;
     @FXML
     private TextField Speed;
-
-    private double blockSize;
-    private boolean sorting = false;
-
-    private GraphicsContext gc;
     private List<Integer> arrayList;
-
     private Queue<Move> moves;
-
     private Object sortingStrategy;
-    private double delay = 0.01;
-    private boolean strokeExist = true;
-    private final String[] sorts = {"BinaryInsertionSort", "BitonicSort", "BogoSort", "BubbleSort",
-            "CocktailShakerSort", "CombSort", "CountingSort", "DoubleSelectionSort", "GnomeSort",
-            "HeapSort", "InPlaceMergeSort", "InsertionSort", "MergeSort", "OddEvenSort",
-            "OptimizedBubbleSort", "PancakeSort", "QuickSort", "SelectionSort", "ShellSort", "StoogeSort"};
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -59,48 +46,38 @@ public class MainController implements Initializable {
                         .then("-fx-prompt-text-fill: transparent;")
                         .otherwise("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);"));
 
-        gc = theCanvas.getGraphicsContext2D();
+
+        canvasUtils = new CanvasUtils(theCanvas);
     }
 
-    private void drawArray() {
-        gc.setFill(Color.BLACK);
-
-        if (strokeExist) gc.setStroke(Color.rgb(244, 244, 244));
-        else gc.setStroke(Color.BLACK);
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            gc.fillRect(i * blockSize, 0, blockSize, arrayList.get(i));
-            gc.strokeRect(i * blockSize, 0, blockSize, arrayList.get(i));
-        }
-    }
 
     @FXML
     private void startSorting() throws InterruptedException {
-        if(sorting) return;
-        else sorting = true;
+        if (canvasUtils.isSorting()) return;
+        else canvasUtils.setSorting(true);
 
         if (SortComboBox.getValue() == null) {
             sortingStrategy = new MergeSort<>();
             SortComboBox.setValue("MergeSort");
         }
 
-        gc.clearRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
+        canvasUtils.clear();
 
         int size = 50;
         if (!dataSize.getText().equals("")) size = Integer.parseInt(dataSize.getText());
-        strokeExist = size <= 200;
+        canvasUtils.setStrokeExist(size <= 200);
 
-        delay = delay / Double.parseDouble(Speed.getText());
-        if (delay < 0.0001) delay = 0.0001;
+        canvasUtils.setDelay((canvasUtils.getDelay()) / Double.parseDouble(Speed.getText()));
+        if (canvasUtils.getDelay() < 0.0001) canvasUtils.setDelay(0.0001);
 
         Random rd = new Random();
         arrayList = new ArrayList<>();
         for (int i = 0; i < size; i++)
             arrayList.add((int) ((Math.abs(rd.nextInt()) + 1) % theCanvas.getHeight()));
 
-        blockSize = theCanvas.getWidth() / arrayList.size();
-
-        drawArray();
+        canvasUtils.setBlockSize(theCanvas.getWidth() / arrayList.size());
+        canvasUtils.setData(arrayList);
+        canvasUtils.drawArray();
 
         String className = SortComboBox.getValue();
         try {
@@ -109,104 +86,15 @@ public class MainController implements Initializable {
                     .getDeclaredConstructor().newInstance();
 
         } catch (InstantiationException | MalformedURLException | ClassNotFoundException | NoSuchMethodException |
-                 InvocationTargetException | IllegalAccessException e) {
+                InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
-
 
         Thread sort = new Thread(new Sorting(arrayList, Comparator.naturalOrder(), (SortAttributes<Integer>) sortingStrategy, moves));
         sort.start();
 
         Thread.sleep(100);
-        startDrawSorting();
-    }
-
-    private void startDrawSorting() {
-        Timeline timeline1;
-        Timeline timeline2;
-
-        if (moves.isEmpty()) return;
-
-        Move current = moves.poll();
-        if (current.pivot) timeline1 = coloredTimeLine(current, Color.GOLD);
-        else timeline1 = coloredTimeLine(current, Color.RED);
-
-        gc.setFill(Color.BLACK);
-        timeline2 = coloredTimeLine(current, Color.BLACK);
-        Timeline finalTimeline = timeline2;
-        timeline1.setOnFinished(e -> finalTimeline.play());
-
-        timeline1.play();
-
-        while (!moves.isEmpty()) {
-            current = moves.poll();
-
-            if (current.pivot) timeline1 = coloredTimeLine(current, Color.GOLD);
-            else timeline1 = coloredTimeLine(current, Color.RED);
-
-            Timeline finalTimeline1 = timeline1;
-            timeline2.setOnFinished(e -> finalTimeline1.play());
-
-            timeline2 = coloredTimeLine(current, Color.BLACK);
-            Timeline finalTimeline2 = timeline2;
-            timeline1.setOnFinished(e -> finalTimeline2.play());
-        }
-        timeline2.setOnFinished(e -> end());
-    }
-
-    private void end() {
-
-        if (strokeExist) gc.setStroke(Color.rgb(244, 244, 244));
-        else gc.setStroke(Color.GREEN);
-
-        Timeline timeline1;
-        Timeline timeline2;
-
-        gc.setFill(Color.GREEN);
-        timeline1 = coloredTimeLine(new Move(0, 0, arrayList.get(0), 0, false, true), Color.GREEN);
-        timeline2 = coloredTimeLine(new Move(1, 0, arrayList.get(1), 0, false, true), Color.GREEN);
-        Timeline finalTimeline = timeline2;
-        timeline1.setOnFinished(e -> finalTimeline.play());
-        timeline1.play();
-
-        for (int i = 2; i < arrayList.size(); i++) {
-            timeline1 = coloredTimeLine(new Move(i, 0, arrayList.get(i), 0, false, true), Color.GREEN);
-            Timeline finalTimeline1 = timeline1;
-            timeline2.setOnFinished(e -> finalTimeline1.play());
-            timeline2 = timeline1;
-        }
-        sorting = false ;
-    }
-
-    private Timeline coloredTimeLine(Move current, Color color) {
-        if (current.singleCol) {
-            return new Timeline(
-                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, theCanvas.getHeight())),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightI))
-            );
-        } else if (current.swap) {
-            return new Timeline(
-                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.j * blockSize, 0, blockSize, current.heightJ)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.j * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightJ)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.j * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightJ))
-            );
-        } else {
-            return new Timeline(
-                    new KeyFrame(Duration.seconds(delay), e -> gc.setFill(color)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.i * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.clearRect(current.j * blockSize, 0, blockSize, current.heightJ)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.i * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.i * blockSize, 0, blockSize, current.heightI)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.fillRect(current.j * blockSize, 0, blockSize, current.heightJ)),
-                    new KeyFrame(Duration.seconds(delay), e -> gc.strokeRect(current.j * blockSize, 0, blockSize, current.heightJ))
-            );
-        }
+        canvasUtils.startDrawSorting(moves);
     }
 
     @FXML
